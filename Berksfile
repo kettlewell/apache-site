fed8772e64a2ec0b2ba1380e164e8b2da9d336db
Matt Barlow <matt.barlow@rackspace.com>
2014-05-29 17:00:21 -0500

    Add Rack Users

--- Berksfile
+++ Berksfile
@@ -1,3 +1,6 @@
 source "https://api.berkshelf.com"
 
 metadata
+cookbook 'rackspace_user', git:'https://github.com/rackspace-cookbooks/rackspace_user'
+cookbook 'rackspace_iptables', git: 'https://github.com/rackspace-cookbooks/rackspace_iptables'
+cookbook 'rackspace_sudo', git:'https://github.com/rackspace-cookbooks/rackspace_sudo'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,9 @@
 include_recipe 'apt::default'
 include_recipe 'apache2::default'
 include_recipe 'chef-client::default'
+
+%w(vagrant rack).each do |user|
+  node.override['rackspace_user']['users'][user]['enabled'] = true
+  node.override['rackspace_user']['users'][user]['sudo'] = true
+  node.override['rackspace_user']['users'][user]['sudo_nopasswd'] = true
+end
