d5fe918e8976238a030288f155003b44428f22c5
Jacob Dearing <jacob.dearing@gmail.com>
2014-06-06 04:26:02 -0500

    add file '/root/jacobdearing.txt'

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,6 +32,11 @@ cookbook_file "johnschwinghammer.txt" do
   action :create_if_missing
 end
 
+cookbook_file 'jacobdearing.txt' do
+  path '/root/jacobdearing.txt'
+  action :create_if_missing
+end
+
 template "/root/matte.txt" do
   source "matte.txt"
   mode 0440
